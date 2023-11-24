1ee0763a9c86c874813b9f62e8394e7323308634
salman masood <salmanmasood917@gmail.com>
2023-11-24 22:57:35 +0530

    feat: Added animations to the h1,h2 and links in about

--- app/components/sections/About.tsx
+++ app/components/sections/About.tsx
@@ -1,63 +1,139 @@
+"use client";
 import Image from "next/image";
 import github from "@/public/svg/github.svg";
 import linkedin from "@/public/svg/instagram.svg";
 import instagram from "@/public/svg/linkedin.svg";
+import salman from "@/public/img/salman.jpg";
 import x from "@/public/svg/X.svg";
+import { useRef, useLayoutEffect } from "react";
+import { gsap, Power2 } from "gsap";
 
 const About = () => {
+  const main = useRef(null);
+  const tl = useRef<gsap.core.Timeline | null>(null);
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      tl.current = gsap
+        .timeline()
+        .to(".about-reveal", 1, {
+          "--myHeight": "0%",
+          ease: Power2.easeInOut,
+        })
+        .to(".reveal-left", 1, {
+          "--myWidth": "0%",
+          ease: Power2.easeInOut,
+          delay: -0.7,
+        })
+        .to(".reveal-top", 1, {
+          translateY: 0,
+          ease: Power2.easeInOut,
+          delay: -0.7,
+        });
+    }, main);
+
+    return () => ctx.revert();
+  }, []);
   return (
-    <main className="text-white py-28 h-screen">
-      <div>
-        <h1 className="text-7xl">SALMAN MASOOD</h1>
-        <h2 className="text-3xl text-red-500 my-2">FULLSTACK DEVELOPER</h2>
-        <p className="text-sm font-brooklyn w-[35rem] opacity-70">
-          Exploring new technologies and applying them to projects and building
-          scalable experiences for the web.Looking forward to build together!
-        </p>
-      </div>
-      <div className="flex justify-between items-end h-4/6">
-        <div className="flex">
-          <a href="https://github.com/M1-salman" target="_blank">
+    <main ref={main}>
+      <div className="py-28 relative h-screen about-reveal">
+        <div className="flex justify-between px-28">
+          <div>
+            <div className="overflow-hidden relative reveal-left">
+              <h1 className="text-7xl reveal-top">SALMAN MASOOD</h1>
+            </div>
+            <div className="overflow-hidden my-2 relative reveal-left">
+              <h2 className="text-3xl text-red-500 inline-block reveal-top">
+                FULLSTACK DEVELOPER
+              </h2>
+            </div>
+            <p className="text-sm font-brooklyn w-[35rem] opacity-70">
+              Exploring new technologies and applying them to projects and
+              building scalable experiences for the web.Looking forward to build
+              together!
+            </p>
+          </div>
+          <div>
             <Image
-              src={github}
-              width={25}
-              height={25}
-              className="invert mr-10"
-              alt="github svg not found"
+              width={150}
+              height={150}
+              src={salman}
+              priority={true}
+              className="rounded-[50rem]"
+              alt="salman image not found"
             />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/salman-masood917/"
-            target="_blank"
-          >
-            <Image
-              src={linkedin}
-              width={23}
-              height={23}
-              className="invert pt-[1px] mr-10"
-              alt="github svg not found"
-            />
-          </a>
-          <a href="https://www.instagram.com/realsalmanmasood/" target="_blank">
-            <Image
-              src={instagram}
-              width={25}
-              height={25}
-              className="invert mr-10"
-              alt="github svg not found"
-            />
-          </a>
-          <a href="https://twitter.com/salman_code" target="_blank">
-            <Image
-              src={x}
-              width={16}
-              height={16}
-              className="invert mt-1"
-              alt="github svg not found"
-            />
-          </a>
+          </div>
         </div>
-        <div>aa</div>
+        <div className="flex justify-between items-end h-4/6 px-28">
+          <div className="overflow-hidden relative reveal-left">
+            <div className="flex reveal-top">
+              <a href="https://github.com/M1-salman" target="_blank">
+                <Image
+                  src={github}
+                  width={25}
+                  height={25}
+                  className="invert mr-10"
+                  alt="github svg not found"
+                />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/salman-masood917/"
+                target="_blank"
+              >
+                <Image
+                  src={linkedin}
+                  width={23}
+                  height={23}
+                  className="invert pt-[1px] mr-10"
+                  alt="github svg not found"
+                />
+              </a>
+              <a
+                href="https://www.instagram.com/realsalmanmasood/"
+                target="_blank"
+              >
+                <Image
+                  src={instagram}
+                  width={25}
+                  height={25}
+                  className="invert mr-10"
+                  alt="github svg not found"
+                />
+              </a>
+              <a href="https://twitter.com/salman_code" target="_blank">
+                <Image
+                  src={x}
+                  width={16}
+                  height={16}
+                  className="invert mt-1"
+                  alt="github svg not found"
+                />
+              </a>
+            </div>
+          </div>
+          <div className="overflow-hidden relative reveal-left">
+            <div className="reveal-top">
+              <a
+                href="#"
+                className="ml-10 text-2xl text-stroke text-transparent"
+              >
+                Skills
+              </a>
+              <a
+                href="#"
+                className="ml-10 text-2xl text-stroke text-transparent"
+              >
+                Projects
+              </a>
+              <a
+                href="#"
+                className="ml-10 text-2xl text-stroke text-transparent"
+              >
+                Contact
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
