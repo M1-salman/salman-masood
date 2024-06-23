c7f3a10c1b69b76560e23db1aac668a2bf17555a
Salman Masood <salmanmasood917@gmail.com>
2024-06-23 12:55:23 +0530

    fix: og-image url

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://collabard.vercel.app"),
+  metadataBase: new URL("https://salman-masood.vercel.app"),
   title: "salman_code",
   description: "Salman masood, a full stack developer",
 };
